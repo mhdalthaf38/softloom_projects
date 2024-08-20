8e355ed8dce55546d6d9298143440e941d360856
mhdal <mhdalthaf38@gmail.com>
2024-08-20 13:34:58 +0530

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           body: TweenandcontrollerAnimation(),
