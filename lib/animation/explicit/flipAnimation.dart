0b3fbc83b93fb0bdde1acbffe028358588219b7f
mhdal <mhdalthaf38@gmail.com>
2024-08-28 13:55:05 +0530

    alignment

--- lib/animation/explicit/flipAnimation.dart
+++ lib/animation/explicit/flipAnimation.dart
@@ -37,66 +37,62 @@ class _FlipanimationState extends State<Flipanimation>
   Widget build(BuildContext context) {
     final rotateY = _animation.value * 3.141;
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                if (_controller.isCompleted) {
-                  _controller.reverse();
-                } else {
-                  _controller.forward();
-                }
-                setState(() {
-                  rotate = !rotate;
-                });
-              },
-              child: Center(
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Transform(
-                        transform: Matrix4.identity()
-                          ..setEntry(3, 2, .001)
-                          ..rotateY(rotateY),
-                        alignment: Alignment.center,
-                        child: _animation.value < .5
-                            ? Container(
-                                width: 200,
-                                height: 200,
-                                color: Color.fromARGB(255, 226, 186, 6),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.dangerous,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              )
-                            : SizedBox.shrink()),
-                    Transform(
-                        transform: Matrix4.identity()
-                          ..setEntry(3, 2, .001)
-                          ..rotateY(rotateY),
-                        alignment: Alignment.center,
-                        child: _animation.value >= .5
-                            ? Container(
-                                width: 200,
-                                height: 200,
-                                color: Color.fromARGB(255, 7, 152, 19),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.person,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              )
-                            : SizedBox.shrink()),
-                  ],
-                ),
-              ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            if (_controller.isCompleted) {
+              _controller.reverse();
+            } else {
+              _controller.forward();
+            }
+            setState(() {
+              rotate = !rotate;
+            });
+          },
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, .001)
+                      ..rotateY(rotateY),
+                    alignment: Alignment.center,
+                    child: _animation.value < .5
+                        ? Container(
+                            width: 200,
+                            height: 200,
+                            color: Color.fromARGB(255, 226, 186, 6),
+                            child: Center(
+                              child: Icon(
+                                Icons.dangerous,
+                                color: Colors.black,
+                              ),
+                            ),
+                          )
+                        : SizedBox.shrink()),
+                Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, .001)
+                      ..rotateY(rotateY),
+                    alignment: Alignment.center,
+                    child: _animation.value >= .5
+                        ? Container(
+                            width: 200,
+                            height: 200,
+                            color: Color.fromARGB(255, 7, 152, 19),
+                            child: Center(
+                              child: Icon(
+                                Icons.person,
+                                color: Colors.black,
+                              ),
+                            ),
+                          )
+                        : SizedBox.shrink()),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
